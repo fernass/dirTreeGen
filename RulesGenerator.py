f8cbc8f7ceee535e88ea933b79cb864cd8dfb75b
fernass <fernass.daoud@web.de>
2015-10-23 11:55:28 +0200

    New Feature: possibility to add a prefix to the file names in the rules line

--- RulesGenerator.py
+++ RulesGenerator.py
@@ -1,32 +1,42 @@
-__author__ = 'fernass daoud'
 import os
 
-class rule:
+__author__ = 'fernass daoud'
+
+
+class Rule:
     def __init__(self, line, line_no):
+        self.prefix = ""
         keys = line.split("-")
-        for i in keys:
+        if keys[0].isalpha():
+            self.prefix = keys[0]
+            del(keys[0])
+
+        if len(keys) < 2:
+            raise SyntaxError("Only one Argument on line {}. At least two Expected".format(line_no))
+        elif len(keys) > 3:
+            raise SyntaxError("More Than Three Arguments on line {}. At Maximum Three Expected".format(line_no))
+        elif len(keys) == 2:
             try:
-                isinstance(int(i),int)
+                self.start = int(keys[0].strip())
+                self.end = int(keys[1].strip())
+                self.incr = 1
             except:
                 raise SyntaxError("None Integer Values on Line No. {} of Input File".format(line_no))
-        if(len(keys)<2):
-            raise SyntaxError("Only one Argument on line {}. At least two Expected".format(line_no))
-        elif(len(keys)>3):
-            raise SyntaxError("More Than Three Arguments on line {}. At Maximum Three Expected".format(line_no))
-        elif(len(keys) == 2):
-            self.start = int(keys[0].strip())
-            self.end = int(keys[1].strip())
-            self.incr = 1
         else:
-            self.start = int(keys[0].strip())
-            self.end = int(keys[1].strip())
-            self.incr = int(keys[2].strip())
+            try:
+                self.start = int(keys[0].strip())
+                self.end = int(keys[1].strip())
+                self.incr = int(keys[2].strip())
+            except:
+                raise SyntaxError("None Integer Values on Line No. {} of Input File".format(line_no))
+
         return
+
 
 class RulesGenerator:
     def __init__(self, cmd_obj):
         try:
-            self.file = open(cmd_obj.args.inputfile,mode='r')
+            self.file = open(cmd_obj.args.inputfile, mode='r')
         except:
             print("Can't access input file!")
             raise PermissionError("Either Access Rights are Granted, or File is not Available")
@@ -38,26 +48,25 @@ class RulesGenerator:
         self.file.seek(0)
         line_no = 0
         for line in self.file:
-            line_no = line_no + 1
-            if(line.find("#")==0):
+            line_no += 1
+            if line.find("#") == 0:
                 continue
-            new_rule = rule(line,line_no)
+            new_rule = Rule(line, line_no)
             self.rules.append(new_rule)
 
     def __iter__(self):
-        self.index =0
+        self.index = 0
         return self
 
     def __next__(self):
-        if self.index <len(self.rules):
-            self.index = self.index + 1
+        if self.index < len(self.rules):
+            self.index += 1
             return self.rules[self.index - 1]
         else:
             raise StopIteration
 
     def apply(self):
         for R in self:
-            for dir in range(R.start,R.end,R.incr):
-                os.mkdir(str(dir),mode=0o775)
+            for directory in range(R.start, R.end, R.incr):
+                os.mkdir(R.prefix + str(directory), mode=0o775)
         return
-
